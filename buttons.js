2f471b26fb2a90fb6405b76d2604d25ef4acea06
Andreya <n01611541@humber.ca>
2023-11-03 17:48:30 -0400

    lab-07

---
+++ buttons.js
@@ -0,0 +1,23 @@
+function onChristmas() {
+    document.getElementById("christmas").style.display = "block";
+}
+
+function hideChristmas() {
+    document.getElementById("christmas").style.display = "none";
+}
+
+function onHalloween() {
+    document.getElementById("halloween").style.display = "block";
+}
+
+function hideHalloween() {
+    document.getElementById("halloween").style.display = "none";
+}
+
+function onNewYears(){
+    document.getElementById("newYears").style.display = "block";
+}
+
+function hideNewYears(){
+    document.getElementById("newYears").style.display = "none";
+}
